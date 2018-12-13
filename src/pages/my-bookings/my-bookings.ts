067c4fdb509ab3987cbd2e38556402d59faec394
Fabian <freicher@stud.hs-heilbronn.de>
2018-12-13 15:29:08 +0100

    Buchen von Autos ist nun möglich.

---
+++ src/pages/my-bookings/my-bookings.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+import {AlertController, App, IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AngularFireDatabase} from "angularfire2/database"
+import {AngularFirestore, AngularFirestoreCollection} from "angularfire2/firestore"
+import firebase from "firebase";
+import {LoginPage} from "../login/login";
+import {ProtocolPage} from "../about/protocol";
+
+
+interface Booking {
+    carID: string;
+    dateEnd: string;
+    dateStart: string;
+    timeEnd: string;
+    timeStart: string;
+    seat: number;
+}
+
+interface Car {
+  farbe: string;
+  modell: string;
+  reserviert: number;
+  sitze: number;
+  kennzeichen: string;
+  gebucht: [string, string, string, string, string]
+
+}
+
+@IonicPage()
+@Component({
+  selector: 'page-my-bookings',
+  templateUrl: 'my-bookings.html',
+})
+export class MyBookingsPage {
+  public bookingCollectionRef: AngularFirestoreCollection<Booking> = this.af.collection(
+    "bookings");
+  public bookings = this.bookingCollectionRef.valueChanges();
+  public carCollectionRef: AngularFirestoreCollection<Car> =  this.af.collection("cars");
+  public cars = this.carCollectionRef.valueChanges();
+  public date: string = new Date().toDateString();
+  bookings1: string = "currentbookings";
+  constructor(public navCtrl: NavController,
+              public app: App,
+              public alertCtrl: AlertController,
+              public toastCtrl: ToastController,
+              private formBuilder: FormBuilder,
+              private af: AngularFirestore,
+              public db: AngularFireDatabase) {
+  }
+  logoutUser(): Promise<void> {
+    this.navCtrl.push(LoginPage);
+    return firebase.auth().signOut();
+    //this.auth.logout();
+    //this.authData.logoutUser();
+  };
+
+  checkuID(){
+    return firebase.auth().currentUser.uid;
+
+  }
+  deleteBooking(data){
+    console.log("Buchung löschen");
+        const createToast = this.toastCtrl.create({
+            message: 'Buchung erfolgreich storniert',
+            duration: 3000
+        });
+        let bookRef = this.af.collection('bookings').ref.where('bookingID', '==', data.bookingID);
+        bookRef.get().then((result) => {
+            result.forEach(doc => {
+
+                console.log(doc.id);
+                this.bookingCollectionRef.doc(doc.id).delete();
+            })
+        });
+
+        createToast.present();
+
+  }
+
+  checkCurrentDate(){
+    const curDate = this.date;
+    console.log(this.date);
+    return this.date;
+
+  }
+  goToProtocoll(){
+    this.navCtrl.push(ProtocolPage)
+  }
+
+
+}
